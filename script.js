c7b3af365b4231897863d9c7ad685845dd9c59bc
Alex Basov <bolex.ua@gmail.com>
2020-02-25 09:26:43 +0200

    add bookmarks to the list

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function closeFloater() {
     body.classList.remove('show-floater');
   }
 }
+
+// =======================
+
+const bookmarkList = document.querySelector('.bookmarks-list');
+const bookmarkForm = document.querySelector('.bookmark-form');
+const bookmarkInput = document.querySelector('input[type=text]');
+
+bookmarkForm.addEventListener('submit', createBookrmark);
+
+function createBookrmark (e) {
+  e.preventDefault();
+
+  const title = bookmarkInput.value;
+  const bookmark = document.createElement('a');
+  bookmark.className = 'bookmark';
+  bookmark.textContent = title;
+  bookmark.href = '#';
+  bookmark.target = '_blank';
+  console.log(bookmark);
+
+  bookmarkList.appendChild(bookmark);
+  bookmarkForm.reset();
+}
